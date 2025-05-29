44884b3013aa3cd06b17e752d4ad59f82f7e73a1
MarcoDiIanni9631 <marco.diianni.3@gmail.com>
2025-05-29 11:31:25 +0200

    Gestito unsat con eccezione che interrompe, creata cartella di test, rimozione predicati pred mode query e ignore per iniziare a lavorare con file geneerati da verimap

--- main.pl
+++ main.pl
@@ -1,3 +1,5 @@
+:- use_module(library(dcg/basics)).
+
 :- use_module(library(clpq)).
 :- use_module(solver_clpq).
 
@@ -11,6 +13,45 @@
 :- assert(file_search_path(z3lib, '/home/marco/Desktop/SWIPrologZ3')).
 :- use_module(z3lib(swiplz3)).
 :- use_module(solver_vidal).
+
+
+%Utilities per rimuovere sporcizia 
+
+
+load_clean(File) :-
+    open(File, read, Stream),
+    load_clean_lines(Stream),
+    close(Stream).
+
+load_clean_lines(Stream) :-
+    read_line_to_string(Stream, Line),
+    ( Line == end_of_file ->
+        true
+    ; should_skip_line(Line) ->
+        load_clean_lines(Stream)
+    ; open_string(Line, LineStream),
+      catch(
+          read_term(LineStream, Term, [syntax_errors(dec10)]),
+          _Err,
+          Term = fail
+      ),
+      close(LineStream),
+      ( Term == fail ->
+            load_clean_lines(Stream)
+      ; assertz(Term),
+        load_clean_lines(Stream)
+      )
+    ).
+
+% Skippa le righe che iniziano con :- pred, :- mode, ecc.
+should_skip_line(Line) :-
+    string_codes(Line, Codes),
+    phrase((
+        ":-", blanks,
+        ( "pred" ; "mode" ; "query" ; "ignore" )
+    ), Codes, _).
+
+
 
 % ----------------------------
 % DEFINIZIONE OPERATORI PERSONALIZZATI
@@ -107,20 +148,25 @@ normalize_bool_expr(A, A).
 % Interprete zmi
 % ----------------------------
 
-zmi(Head) :- zmi(Head, 100000).
+zmi(Head) :-
+    catch(zmi(Head, 100000), z3_unsat, (
+        writeln(' Esecuzione interrotta: vincoli insoddisfacibili (Z3 UNSAT).'),
+        fail
+    )).
 
 zmi(Head, MaxSteps) :-
 	InitialZ3 = true,
 	InitialCLPQ = true,
-	(zmi_aux(Head, InitialZ3, InitialCLPQ, MaxSteps, FinalZ3, FinalCLPQ, Tree),
-		nl, writeln('--- Derivation Tree ---'),
-		print_tree(Tree),
-		nl, writeln('--- CLPQ Constraints ---'),
-		normalize_bool_expr(FinalCLPQ, NormalizedCLPQ),
-		conj_to_list(NormalizedCLPQ, CLPQList),
-		writeln(CLPQList),
-		nl, writeln('--- FINAL MODEL (Z3) ---'),
-		z3_print_model_final(FinalZ3)).
+	zmi_aux(Head, InitialZ3, InitialCLPQ, MaxSteps, FinalZ3, FinalCLPQ, Tree),
+	nl, writeln('--- Derivation Tree ---'),
+	print_tree(Tree),
+	nl, writeln('--- CLPQ Constraints ---'),
+	normalize_bool_expr(FinalCLPQ, NormalizedCLPQ),
+	conj_to_list(NormalizedCLPQ, CLPQList),
+	writeln(CLPQList),
+	nl, writeln('--- FINAL MODEL (Z3) ---'),
+	z3_print_model_final(FinalZ3).
+
 
 zmi_aux(true, Z3, CLPQ, _, Z3, CLPQ, true).
 zmi_aux((A, B), Z3In, CLPQIn, Steps, Z3Out, CLPQOut, (TreeA, TreeB)) :-
@@ -136,13 +182,16 @@ zmi_aux(constr(C), Z3In, CLPQIn, _, Z3Out, CLPQOut, constr(Normalized)) :-
 	nl, writeln('--- Immediate CLPQ Check ---'),
 	clpq_sat_from_list(FilteredCLPQ),
 	nl, writeln('--- Immediate Z3 Check ---'),
-	z3_sat_check(Z3Out, Res),
-	( Res == unsat ->
-		writeln('Z3 result: UNSAT'),
-		writeln('Vincolo insoddisfacibile. Interrompo.'), !, fail
-	; Res == sat ->
-		writeln('Z3 result: SAT'), true
-	; writeln('Z3 result: UNKNOWN or push_failed'), true ).
+z3_sat_check(Z3Out, Res),
+( Res == unsat ->
+  (  writeln('Z3 result: UNSAT'),
+    writeln('Vincolo insoddisfacibile. Interrompo.'),
+    throw(z3_unsat))
+; Res == sat ->
+    writeln('Z3 result: SAT')
+; writeln('Z3 result: UNKNOWN or push_failed')
+).
+
 
 zmi_aux(Head, Z3In, CLPQIn, Steps, Z3Out, CLPQOut, SubTree => Head) :-
 	Steps > 0,
@@ -150,15 +199,9 @@ zmi_aux(Head, Z3In, CLPQIn, Steps, Z3Out, CLPQOut, SubTree => Head) :-
 	Head \= (_, _),
 	Head \= constr(_),
 	NewSteps is Steps - 1,
-	(clause(Head, Body) ->
-		zmi_aux(Body, Z3In, CLPQIn, NewSteps, Z3Out, CLPQOut, SubTree)
-	; is_qr_constr(Head) -> (
-		build_conjunct([CLPQIn, Head], CLPQOut),
-		build_conjunct([Z3In, Head], Z3Out),
-		SubTree = constr(Head))
-	; (CLPQOut = CLPQIn,
-		build_conjunct([Z3In, Head], Z3Out),
-		SubTree = Head)).
+    clause(Head, Body),
+    zmi_aux(Body, Z3In, CLPQIn, NewSteps, Z3Out, CLPQOut, SubTree).
+	
 		
 
 zmi_aux(_, _, _, 0, _, _, _) :-
@@ -178,57 +221,3 @@ print_tree(SubTree => Head, Indent) :-
 	NewIndent is Indent + 2,
 	print_tree(SubTree, NewIndent).
 print_tree(Other, Indent) :- tab(Indent), writeln(Other).
-
-% ----------------------------
-% Esempi di test
-% ----------------------------
-
-example_and :- constr(X > 0 & Y < 2).
-example_not :- constr(not(X =< 3)).
-example_tilde :- constr(~(Y > 5)).
-example_mixed :- constr(~(A > 0) & (B < 2 & (C = true & D = false))).
-example_eq_true :- constr((X > 1) = true).
-example_eq_false :- constr((Y < 5) = false).
-example_eq_true2 :- constr(A = true).
-
-example_and_2(X, Y) :- constr(and(X > 0, Y < 1)).
-
-sumlistZ([], X) :- constr(X = 0).
-sumlistZ([H | T], R) :- sumlistZ(T, R1), constr(R = R1 + H).
-example_sumlistZ(R) :- sumlistZ([1, 2], R).
-
-run_all_tests :-
-	nl, writeln(">> Test: example_and"), zmi(example_and),
-	nl, writeln(">> Test: example_not"), zmi(example_not),
-	nl, writeln(">> Test: example_tilde"), zmi(example_tilde),
-	nl, writeln(">> Test: example_eq_true"), zmi(example_eq_true),
-	nl, writeln(">> Test: example_eq_true2"), zmi(example_eq_true2),
-	nl, writeln(">> Test: example_eq_false"), zmi(example_eq_false).
-
-%Test Vidal (per riferimento)
-
-example_simple(X) :- constr(X > 5).
-example_and(X, Y) :- constr((X > 0, Y < 1)).
-example_and_unsat(X) :- constr((X > 0, X < 0)).
-example_and_or(X, Y) :- constr(and(X > 0, or(Y = 3, Y = 5))).
-
-
-% Predicato ausiliario: raddoppia un numero
-double(X, Y) :- constr(Y = 2 * X).
-
-% Caso base: la somma della lista vuota è 0
-double_sum([], R) :- constr(R = 0).
-
-% Caso ricorsivo: raddoppia H, somma ricorsiva su T, poi somma i risultati
-double_sum([H | T], R) :-
-    double(H, DH),               % chiamata al terzo predicato
-    double_sum(T, RT),
-    constr(R = DH + RT).
-
-% Esempio di test
-example_double_sum(R) :- double_sum([1, 2], R).
-
-example_or_verimap :- constr((X = 1 v X = 2)).
-example_or_alt :- constr((Y = 10 or Y = 20)).
-
-example_or_mixed :- constr(((X > 0 & Y < 3) v (X < 0 & Y > 10))).
