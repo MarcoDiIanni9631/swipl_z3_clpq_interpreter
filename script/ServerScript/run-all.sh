61e1676cc575c4a9448fbe8a39e9fe399601ecce
MarcoDiIanni9631 <marco.diianni.3@gmail.com>
2025-10-17 12:19:29 +0200

    Aggiunti i file commentati che danno errore nella valutazione dell'interprete dei file Lia-Lin.

---
+++ script/ServerScript/run-all.sh
@@ -0,0 +1,3 @@
+bash -c "cd /home/labeconomia/mdiianni/verimap_projects/chc-comp25-benchmarks && bash run-one.sh LIA-Arrays.set"
+bash -c "cd /home/labeconomia/mdiianni/verimap_projects/chc-comp25-benchmarks && bash run-one.sh LIA-Lin-Arrays.set"
+bash -c "cd /home/labeconomia/mdiianni/verimap_projects/chc-comp25-benchmarks && bash run-one.sh LIA.set"
